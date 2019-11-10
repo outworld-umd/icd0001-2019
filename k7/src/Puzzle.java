325a5e257ddf10472cae285fadb35cc59a66caa5
arpahh <arpahh@ttu.ee>
2019-11-10 22:37:08 +0200

    readme.md

--- k7/src/Puzzle.java
+++ k7/src/Puzzle.java
@@ -1,43 +1,51 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class Puzzle {
 
     private HashMap<Character, Integer> solution = new HashMap<>();
     private ArrayList<String> solutions = new ArrayList<>();
+    private String add1;
+    private String add2;
+    private String sum;
 
     public static void main(String[] input) {
-        ArrayList result = new Puzzle().solve(input);
+        ArrayList<? extends String> result = new Puzzle().solve(input);
         System.out.println(result);
         System.out.println(result.size());
     }
 
     private ArrayList<String> solve(String[] input) {
         if (input.length != 3) throw new RuntimeException("Puzzle solver only takes an array with size 3.");
-        StringBuilder sb = new StringBuilder();
-        for (Character ch : String.join("", input).toCharArray()) {
-            if (sb.indexOf(ch.toString()) == -1) sb.append(ch.toString());
+        Set<String> letters = new LinkedHashSet<>();
+        add1 = input[0];
+        add2 = input[1];
+        sum = input[2];
+        for (int i = 0; i < 10; i++) {
+            if (i < add1.length()) letters.add(Character.toString(add1.charAt(i)));
+            if (i < add2.length()) letters.add(Character.toString(add2.charAt(i)));
+            if (i < sum.length()) letters.add(Character.toString(sum.charAt(i)));
         }
-        if (sb.length() > 10) return solutions;
-        solveRecursively(sb.toString(), input[0], input[1], input[2]);
+        if (letters.size() > 10) return solutions;
+        solveRecursively(String.join("", letters));
         return solutions;
     }
 
-    private void solveRecursively(String availableLetters, String w1, String w2, String w3) {
+    private boolean solveRecursively(String availableLetters) {
         if (availableLetters.isEmpty()) {
-            if (!(solution.get(w1.charAt(0)) == 0 || solution.get(w2.charAt(0)) == 0 || solution.get(w3.charAt(0)) == 0)
-                    && valueOf(w1) + valueOf(w2) == valueOf(w3)) {
+            if (!(solution.get(add1.charAt(0)) == 0 || solution.get(add2.charAt(0)) == 0 || solution.get(sum.charAt(0)) == 0)
+                    && valueOf(add1) + valueOf(add2) == valueOf(sum)) {
                 solutions.add(solution.toString());
             }
-            return;
+            return false;
         }
         for (int digit = 0; digit < 10; digit++) {
             if (!solution.containsValue(digit)) {
                 solution.put(availableLetters.charAt(0), digit);
-                solveRecursively(availableLetters.substring(1), w1, w2, w3);
+                if (solveRecursively(availableLetters.substring(1))) return true;
                 solution.remove(availableLetters.charAt(0));
             }
         }
+        return false;
     }
 
     private int valueOf(String w) {
--- k7/test/PuzzleTest.java
+++ k7/test/PuzzleTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class PuzzleTest {
 
    @Test (timeout=20000)
-   public void test1() { 
+   public void test1() {
       Puzzle.main (new String[]{"YKS", "KAKS", "KOLM"}); // 234 solutions
       Puzzle.main (new String[]{"SEND", "MORE", "MONEY"}); // 1 solution
       Puzzle.main (new String[]{"ABCDEFGHIJAB", "ABCDEFGHIJA", "ACEHJBDFGIAC"});  // 2 solutions

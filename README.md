a5d2018327e0222b5caade308b1168ee43634479
arpahh <arpahh@ttu.ee>
2019-11-26 03:54:54 +0200

    tested 2000 graph + added timer

--- README.md
+++ README.md
@@ -8,4 +8,4 @@
 | **Kodutöö 4** | Abstraktsed andmetüübid  | [Lfraction](k4/src/Lfraction.java)         | kaitstud |    4/4  |
 | **Kodutöö 5** | Puud                     | [Tnode](k5/src/Tnode.java)                 | kaitstud |    8/8  |
 | **Kodutöö 6** | Graafid                  | [GraphTask](k6/src/GraphTask.java)         |  tehtud  |   0/16  |
-| **Kodutöö 7** | Sõned                    | [Puzzle](k7/src/Puzzle.java)               |  tehtud  |   0/10  |                                       |          |   0/10  |
+| **Kodutöö 7** | Sõned                    | [Puzzle](k7/src/Puzzle.java)               |  tehtud  |   0/10  |
--- k6/src/GraphTask.java
+++ k6/src/GraphTask.java
@@ -16,7 +16,11 @@ public class GraphTask {
      * Actual main method to run examples and everything.
      */
     public void run() {
-        // TODO!! Your tests here!
+        Graph graph = new Graph("G");
+        graph.createRandomSimpleGraph(2000, 2000);
+        long start = System.currentTimeMillis();
+        System.out.println(graph.createTransitiveClosure());
+        System.out.println("Time elapsed: " + (System.currentTimeMillis() - start) / 1000D + " s");
     }
 
     /**
@@ -133,9 +137,29 @@ public class GraphTask {
         }
 
         /**
+         * Create a connected undirected random tree with n vertices.
+         * Each new vertex is connected to some random existing vertex.
+         * @param n number of vertices added to this graph
+         */
+        public void createRandomTree (int n) {
+            if (n <= 0)
+                return;
+            Vertex[] varray = new Vertex [n];
+            for (int i = 0; i < n; i++) {
+                varray [i] = createVertex ("v" + String.valueOf(n-i));
+                if (i > 0) {
+                    int vnr = (int)(Math.random()*i);
+                    createArc ("a" + varray [vnr].toString() + "_"
+                            + varray [i].toString(), varray [vnr], varray [i]);
+                    createArc ("a" + varray [i].toString() + "_"
+                            + varray [vnr].toString(), varray [i], varray [vnr]);
+                } else {}
+            }
+        }
+
+        /**
          * Create an adjacency matrix of this graph.
          * Side effect: corrupts info fields in the graph
-         *
          * @return adjacency matrix
          */
         public int[][] createAdjMatrix() {
@@ -158,6 +182,48 @@ public class GraphTask {
                 v = v.next;
             }
             return res;
+        }
+
+        /**
+         * Create a connected simple (undirected, no loops, no multiple
+         * arcs) random graph with n vertices and m edges.
+         * @param n number of vertices
+         * @param m number of edges
+         */
+        public void createRandomSimpleGraph (int n, int m) {
+            if (n <= 0)
+                return;
+            if (n > 2500)
+                throw new IllegalArgumentException ("Too many vertices: " + n);
+            if (m < n-1 || m > n*(n-1)/2)
+                throw new IllegalArgumentException
+                        ("Impossible number of edges: " + m);
+            first = null;
+            createRandomTree (n);       // n-1 edges created here
+            Vertex[] vert = new Vertex [n];
+            Vertex v = first;
+            int c = 0;
+            while (v != null) {
+                vert[c++] = v;
+                v = v.next;
+            }
+            int[][] connected = createAdjMatrix();
+            int edgeCount = m - n + 1;  // remaining edges
+            while (edgeCount > 0) {
+                int i = (int)(Math.random()*n);  // random source
+                int j = (int)(Math.random()*n);  // random target
+                if (i==j)
+                    continue;  // no loops
+                if (connected [i][j] != 0 || connected [j][i] != 0)
+                    continue;  // no multiple edges
+                Vertex vi = vert [i];
+                Vertex vj = vert [j];
+                createArc ("a" + vi.toString() + "_" + vj.toString(), vi, vj);
+                connected [i][j] = 1;
+                createArc ("a" + vj.toString() + "_" + vi.toString(), vj, vi);
+                connected [j][i] = 1;
+                edgeCount--;  // a new edge happily created
+            }
         }
 
         /**
